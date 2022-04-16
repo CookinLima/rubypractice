3ff818d844e471f2781f2e67e502fc401178c540
Kenny Wong <kken.w@hotmail.com>
2022-04-16 19:30:26 +0800

    added conditionals

--- simplecalculator.rb
+++ simplecalculator.rb
@@ -1,5 +1,6 @@
 # A simple calculator based off 2 input variables
-# updated to use method 
+# updated to use method
+# updated to use conditionals
 
 def multiply(num1, num2) 
     num1.to_f * num2.to_f
@@ -21,17 +22,29 @@ def modulus(num1, num2)
     num1.to_f % num2.to_f
 end
 
-
 puts "Simple Calculator"
 20.times {print "-"}
-def calculate(num1, num2) 
+puts
 puts "Enter first number"
 num1 = gets.chomp
 puts "Enter second number"
 num2 = gets.chomp
+puts "Press \'1' to multiply"
+puts "Press \'2' to divide"
+puts "Press \'3' to add"
+puts "Press \'4' to subtract"
+puts "Press \'5' find the remainer"
+task = gets.chomp
+if task == "1"
     puts "The answer is #{multiply(num1,num2)} when multiplied"
+elsif task == "2"
     puts "The answer is #{division(num1,num2)} when divided"
+elsif task == "3"
     puts "The answer is #{addition(num1,num2)} when added"
-    puts "The answer is #{subtraction(num1,num2)} when deducted"
+elsif task = "4"
+    puts "The answer is #{subtraction(num1,num2)} when subtracted"
+elsif task = "5"
     puts "The modulus is #{modulus(num1,num2)}"
+else 
+    puts "Please enter a correct task"
 end
